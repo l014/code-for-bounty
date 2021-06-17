00af5927bd38eb737eb9bfe009ac6fd4d825921b
l014 <74553087+l014@users.noreply.github.com>
2021-06-17 08:53:32 +0000

    Delete lists/ziuto directory

--- lists/ziuto/arj.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-timestamp=$(date +%s)
-u=$1
-arjun -u $1 -oT scanthis/$timestamp  
--- lists/ziuto/bon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-timestamp=$(date +%s)
-u=$1
-mkdir -p scan.out/bitOut
-
-
-echo $u"FUZZ" | ~/go/bin/./BountyIt --header "HOST: host" -t 100 -p lists/zpays | tee -a scan.out/bitOut/$timestamp
